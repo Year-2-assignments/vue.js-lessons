5392a47cfcac2127da0fd0b9699c5669368b5bb4
filippa00 <filippa00@users.noreply.github.com>
2022-02-14 11:27:36 +0100

    commited stock

---
+++ src/components/StockItem.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+
+export default {
+    name: "StockItem",
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/StockList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <h1>Stock list</h1>
+    <div class = "row">
+    <!-- Individual stocks -->
+    <div class="col-md-4 mt-4" v-for="stock in stocks" :key="stock.name">
+      <div class="card">
+        <div class="card-body">
+          <td>{{ stock.name }}</td>
+          <span
+            :class="{
+              down: stock.price < stock.previousPrice,
+              up: stock.price > stock.previousPrice,
+            }"
+            >{{ stock.currency }} {{ stock.price.toFixed(4) }}</span
+          >
+        </div>
+      </div>
+    </div>
+    <!-- End of individual stocks -->
+
+  </div>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {
+  name: "StockList",
+  data() {
+    return {
+      stocks: [
+        { name: "BMW", price: 61.05, previousPrice: 0, currency: "€" },
+        { name: "Caterpillar", price: 146.49, previousPrice: 0, currency: "$" },
+        { name: "AMD", price: 76.5, previousPrice: 0, currency: "$" },
+        { name: "Gazprom", price: 4.583, previousPrice: 0, currency: "$" },
+      ],
+      portfolio: [],
+    };
+  },
+  methods: {
+    updatePrices() {
+      this.stocks.forEach((stock) => {
+        stock.previousPrice = stock.price;
+        stock.price += (Math.random() - 0.5) * 2;
+        if (stock.price < 0) {
+          stock.price = 0;
+        }
+      });
+    },
+  },  
+  mounted() {
+    setInterval(() => {
+      this.updatePrices();
+    }, 1000);
+  },
+};
+</script>
+
+<style scoped>
+.up {
+  color: green;
+}
+.down {
+  color: red;
+}
+</style>
